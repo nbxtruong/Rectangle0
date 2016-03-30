831a350be570dd24118a0499e4475e55b39f8bbb
Nguyen Bao Xuan Truong <omega220@gmail.com>
2016-03-30 23:19:29 +0700

    Frist upload

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Rectangle r = new Rectangle(5, 10);
+		System.out.println("My rectangle has surface " + r.surface()
+				+ " and a perimeter " + r.perimeter());
+
+	}
+
+}
